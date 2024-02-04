c86387e0c48d3c5a014b6e02b27f1872e1a7b300
miwanoff <marina.iva.noffa@gmail.com>
2024-02-04 15:11:36 +0200

    Add file creation

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 require "autoload.php";
 $header = new Header("Заголовок Лендінгу");
 $text = new Text("Це лендинг");
-$model = new Model([$header, $text], "Лендінг");
+$model = new Model([$header, $text, $text], "Лендінг");
 $str_land = $model->generate(); // генерація тексту лендинга
-echo $str_land;
+//echo $str_land;
+
+$dir = "landing";
+//mkdir($dir); // створення каталогу за вказаним шляхом
+$f = fopen("{$dir}/index.html", "w+"); // створення файлу лендингу за вказаним шляхом
+fwrite($f, $str_land); // запис в файл коду лендинга
+fclose($f);
+echo "Лендинг успішно створено!";
